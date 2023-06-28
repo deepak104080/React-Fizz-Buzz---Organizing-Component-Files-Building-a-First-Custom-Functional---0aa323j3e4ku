0bcf404638437d049f9c2885ecd2868e271df1cb
deepak104080 <deepak104080@gmail.com>
2023-06-28 22:50:14 +0530

    assg

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/App.css';
 const App = () => {
 
-  
+  const [count, setCount] = useState(1);
+  const [tempClass, setTempClass] = useState('');
+
+  function increaseFn() {
+    setCount(count => count + 1);
+  }
+
+  function decreaseFn() {
+    setCount(count => count - 1);
+  }
+
+  useEffect(() => {
+    if (count % 5 == 0 && count % 3 == 0) {
+      setTempClass('fizzbuzz')
+    } else if (count % 3 == 0) {
+      setTempClass('fizz')
+    } else if (count % 5 == 0) {
+      setTempClass('buzz')
+    } else {
+      setTempClass('normal')
+    }
+  })
+
   return (
     <div id="main">
-
+      <button onClick={increaseFn}>Increase</button>
+      <div className={tempClass}>{count}</div>
+      <button onClick={decreaseFn}>Decrease</button>
     </div>
   )
 }
-
 
 export default App;
